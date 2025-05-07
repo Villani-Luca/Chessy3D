f9f52a0cc7d458e55c1ce24fa833f2b939154f37
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2025-05-07 09:34:47 +0200

    Retrieval - niente di particolare

--- src/retrieval/src/main__remove_duplicates.py
+++ src/retrieval/src/main__remove_duplicates.py
@@ -7,16 +7,23 @@ import pandas as pd
 import numpy as np
 
 ROOT = pathlib.Path.cwd().parent.parent.parent
-PARQUEET_FOLDER = (ROOT / 'data/retrieval/parqueet')
+PARQUEET_FOLDER = (ROOT / 'data/retrieval/parqueet/0000')
 PARQUEET_OUT_FOLDER = (ROOT / 'data/retrieval/parqueet_out')
 OUTPUT_BATCH_SIZE = 2000
+STARTING_INDEX_OUTPUT = 0
+
+def filter_func(x: pathlib.Path):
+    value = int(x.name.split('.')[0])
+    return 0 <= value <= 200
 
 
 if __name__ == '__main__':
-    dt = pd.concat(pd.read_parquet(parquet_file) for parquet_file in PARQUEET_FOLDER.glob('*.parquet'))
+    #files = [f for f in filter(filter_func, PARQUEET_FOLDER.glob('*.parquet'))]
+    #dt = pd.concat(pd.read_parquet(parquet_file) for parquet_file in files)
+    dt = pd.read_parquet(PARQUEET_FOLDER.as_posix())
     dt.drop_duplicates('id', inplace=True)
 
 
     for index, chunk in enumerate(np.array_split(dt, dt.shape[0] // OUTPUT_BATCH_SIZE)):
-        pd.DataFrame(chunk).to_parquet((PARQUEET_OUT_FOLDER / f'_{index + 1}.parquet'), index=False)
+        pd.DataFrame(chunk).to_parquet((PARQUEET_OUT_FOLDER / f'_{index + 1 + STARTING_INDEX_OUTPUT}.parquet'), index=False)
 
